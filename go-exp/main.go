7f892d4b5e6776006b5ec622739b8c128c1952ad
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2022-11-16 12:00:15 +0700

    feat: minor: Solving an easy tuition-fee calculation problem

--- go-exp/main.go
+++ go-exp/main.go
@@ -9,6 +9,9 @@ import (
 	"time"
 )
 
+// Anouncement: This is the place for daily coding improvement by solving some interesting challenges (almost everyday!).
+
+// 11152022:
 // NOTE: `go help work` := A great leap forward/breakthrough/saltation in Golang for packages
 // and directories management!
 
@@ -40,7 +43,7 @@ func (gs *GenericStruct[T]) CreateFile(fileName string) (*os.File, error) {
 	return file, err
 }
 
-// NOTE: Pointer receiver's generic type-parameter cannot consummed slices as it type.
+// NOTE: Pointer receiver's generic type-parameter cannot consume slices as it type.
 func (gs *GenericStruct[T]) ExportToFile(file *os.File) (int, error) {
 	fileContent := ConvertToPrimitive(gs.Data)
 	numBytes, err := file.Write([]byte(fileContent))
@@ -71,7 +74,67 @@ type EmbeddedStruct struct {
 	_ *GenericStruct[string] `bson:",inline"`
 }
 
+// 11162022:
+// Resource: Interface to string conversion (https://www.golinuxcloud.com/golang-interface-to-string/)
+
+// Problem: Whenever a student levels up to an odd-grade, the tuition fee must be upgraded as follows:
+// Mapping: `[1, 3, 5, 7, etc] -> [100, 100*3, 100*5, 100*7, etc]`, but the adjacent even-grades' tuition fee stays the same.
+
+type GradeTuition map[int]interface{}
+
+func isSlicesContains[T comparable](s []T, e T) bool {
+	for _, v := range s {
+		if v == e {
+			return true
+		}
+	}
+	return false
+}
+
+func (gt GradeTuition) IsKeyExisted(key int) bool {
+	totalGrades := len(gt)
+	keySlices := make([]int, 0, totalGrades)
+	for k := range gt {
+		keySlices = append(keySlices, k)
+	}
+
+	return isSlicesContains(keySlices, key)
+}
+
+func (gt *GradeTuition) CalculateTotalFee(gradeNum int) *GradeTuition {
+	if gradeNum == 0 || gt.IsKeyExisted(0) {
+		return nil
+	}
+
+	var baseTuition = 100
+	for grade := range *gt {
+		if grade == 1 {
+			(*gt)[grade] = baseTuition
+		}
+		if grade%2 == 1 {
+			(*gt)[grade] = baseTuition * grade
+			(*gt)[grade+1] = (*gt)[grade]
+		}
+	}
+
+	return gt
+}
+
+// Core functions:
+func printWithPattern(pattern string, data interface{}) {
+	if strings.Compare("+", pattern) == 0 && data == nil {
+		today := time.Now().String()
+		fmt.Printf("%s %s\n", pattern, today)
+	}
+
+	if strings.Compare("=", pattern) == 0 && data != nil {
+		fmt.Printf("%s>\t%+v\n", strings.Repeat(pattern, 5), data)
+	}
+}
+
 func main() {
+	// 11152022:
+	printWithPattern("+", nil)
 	gs := GenericStruct[string]{
 		Data: "I want something just like this! ~ One Republic",
 		Ctx:  *new(context.Context),
@@ -87,8 +150,32 @@ func main() {
 
 	fileName := "test.txt"
 	newFile, _ := gs.CreateFile(fileName)
-	fmt.Printf("=====>\t%+v\n", newFile)
+	printWithPattern("=", newFile)
 
 	n, _ := gs.ExportToFile(newFile)
-	fmt.Printf("=====>\t%+v\n", n)
+	printWithPattern("=", n)
+
+	// 11162022:
+	printWithPattern("+", nil)
+	expGt := GradeTuition{
+		1: "100",
+		2: "100",
+		3: "300",
+		4: "300",
+		5: "500",
+		6: "500",
+		7: "700",
+	}
+	testKeyExisted := expGt.IsKeyExisted(0)
+	printWithPattern("=", testKeyExisted)
+
+	gradeMap := GradeTuition{
+		1: "",
+		2: "",
+		3: "",
+		4: "",
+		5: "",
+	}
+	printWithPattern("=", gradeMap.CalculateTotalFee(0))
+	printWithPattern("=", gradeMap.CalculateTotalFee(3))
 }
